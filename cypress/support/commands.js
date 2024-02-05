3fefdfa5ba26163ca705370b534381a1ed840efb
Carlos Otávio de Souza <carlosotavio_souza@apple.com>
2024-02-05 14:08:08 -0300

    atualizando projeto

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,7 @@
+/*
+Try to use the chatGPT function to do login via API
+and use the header to nav on the system
+*/
 Cypress.Commands.add("apiLogin", (username, password) => {
   cy.request({
     method: "POST",
